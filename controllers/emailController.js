e0e191747a50846774a765be7a0fe71f32cc0fd0
THEHACKILLER17 <shaikhzaid0903@gmail.com>
2026-05-04 15:50:00 +0530

    changeing the port of nodemailer

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -6,7 +6,7 @@ export const sendInvoiceEmail = async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
+      port: 465,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
